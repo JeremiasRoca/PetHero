488bc67634af0192b2ceda56c6e7f8db326e530b
Jeremias Roca <jereroca2@gmail.com>
2022-10-25 21:19:46 -0300

    Add pet service
    agregado de servicio pet

---
+++ Views/add-files.php
@@ -0,0 +1,28 @@
+<?php require 'header.php'; ?>
+
+
+<form action="<?php echo FRONT_ROOT."Pet/uploadFile"?>" method="post" enctype="multipart/form-data">
+  <div class="container">
+    <h1>More info Pet</h1>
+    <br>    
+    <br>
+    <label for="photo"><b class="p-text">Photo of your pet</b></label>
+    <input type="file" placeholder="Photo of your pet" name="photo" id="Photo" accept=".jpeg,.jpg,.pdf,.gif,.png,.jfif" required>
+    <br>
+    <br>
+    <label for="vaxPlanImg"><b class="p-text">Vaccination plan of your pet</b></label>
+    <input type="file"  placeholder="vaccination plan" name="vaxPlanImg" id="VaxPlanImg" accept=".jpeg,.jpg,.pdf,.gif,.png,.jfif" required>
+    <br>
+    <br>
+    <label for="video"><b class="p-text">Video of your pet</b></label>
+    <input type="file"  placeholder="Video of your pet" name="video" id="Video"  > 
+    <br>
+    <hr>
+    <br>
+    <br>
+    <button type="submit" value="submit"  class="large-button">Add Pet</button>
+  </div>
+</form>
+
+
+<?php require 'footer.php'; ?>
---
+++ Views/footer.php
@@ -0,0 +1,6 @@
+
+<div class="footer">
+    <p >Copyright &copy;PetHero <?php echo date('Y'); ?></p>
+</div>
+</body>
+</html>
